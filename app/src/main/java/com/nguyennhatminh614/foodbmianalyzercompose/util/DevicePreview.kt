5acb90ef6d6da6df4816b4d7f7bc5ed880f014eb
MinhNN2381 <minhnm@piontech.co>
2026-03-11 15:36:35 +0700

    feat(all): @DevicePreview remove device

--- app/src/main/java/com/nguyennhatminh614/foodbmianalyzercompose/util/DevicePreview.kt
+++ app/src/main/java/com/nguyennhatminh614/foodbmianalyzercompose/util/DevicePreview.kt
@@ -10,12 +10,4 @@ import androidx.compose.ui.tooling.preview.Preview
 // Phone
 @Preview(name = "1. Phone - Light", device = Devices.PIXEL_4, showSystemUi = true, showBackground = true)
 @Preview(name = "2. Phone - Dark", device = Devices.PIXEL_4, uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true, showBackground = true)
-
-// Foldable
-@Preview(name = "3. Foldable - Light", device = "spec:width=673dp,height=841dp", showSystemUi = true, showBackground = true)
-@Preview(name = "4. Foldable - Dark", device = "spec:width=673dp,height=841dp", uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true, showBackground = true)
-
-// Tablet
-@Preview(name = "5. Tablet - Light", device = "spec:width=1280dp,height=800dp,dpi=240", showSystemUi = true, showBackground = true)
-@Preview(name = "6. Tablet - Dark", device = "spec:width=1280dp,height=800dp,dpi=240", uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true, showBackground = true)
 annotation class DevicePreview
